8d91ba96e432e2c274df665f16888a79dfe5752a
cortezle <00029117@uca.edu.sv>
2019-05-31 10:42:53 -0600

    tuve que actualizar androidstudio rip f :v

--- app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
+++ app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
@@ -1,11 +1,14 @@
 package com.petrlr14.mvvm.database.viewmodels
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.petrlr14.mvvm.database.RoomDB
 import com.petrlr14.mvvm.database.entities.GitHubRepo
 import com.petrlr14.mvvm.database.repositories.GitHubRepoRepository
+import kotlinx.coroutines.launch
 
 class GitHubRepoViewModel(private val app: Application) : AndroidViewModel(app) {
 
@@ -18,8 +21,41 @@ class GitHubRepoViewModel(private val app: Application) : AndroidViewModel(app) 
 
     private suspend fun insert(repo:GitHubRepo)=repository.insert(repo)
 
-    fun getAll():LiveData<List<GitHubRepo>>{
-        return repository.getAll()
+
+    fun retrieveRepo(user:String)=viewModelScope.launch {
+
+        /**
+         * Primero se borra la tabla de repos
+         **/
+        this@GitHubRepoViewModel.nuke()
+
+        /**
+         * Obtiene el objeto Response de la consulta a la API
+         **/
+        val response=repository.retrieveReposAsync(user).await()
+
+        /**
+         * Evalua y decide dependiendo del estado de la respuesta obtenida
+         **/
+        if(response.isSuccessful) with(response){
+
+            //Inserta toda la lista en la base de datos
+            this.body()?.forEach {
+                this@GitHubRepoViewModel.insert(it)
+            }
+
+        }else with(response){
+
+            println(this.code())
+            when(this.code()){
+                //Aqui pueden evaluarse todos los codigos HTTP
+                404->{
+                    //Muestra un estado de error en caso no encuentre el usuario
+                    Toast.makeText(app, "Usuario no encontrado", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
     }
 
     private suspend fun nuke()= repository.nuke()
